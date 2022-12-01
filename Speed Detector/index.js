4911479189c9d676f988c4af7f8e712d9eef14fc
Miluche <118877950+Miluche@users.noreply.github.com>
2022-12-01 20:16:23 +0300

    Update index.js

--- Speed Detector/index.js
+++ Speed Detector/index.js
@@ -1,1 +1,12 @@
-
+let speed=prompt("please the driver's speed", speed)
+if(speed<70){
+   console.log("Ok");
+}
+else
+{
+console.log("Points:"+((speed-70)/5));
+if(((speed-70)/5)>12)
+{
+sonsole.log("License suspended");
+}
+}
