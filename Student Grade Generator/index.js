35998f2ae49560afe30807f7831c436d6c521760
Miluche <118877950+Miluche@users.noreply.github.com>
2022-12-01 20:39:31 +0300

    Update index.js

--- Student Grade Generator/index.js
+++ Student Grade Generator/index.js
@@ -1,6 +1,7 @@
-//code to display grades
-let mark= prompt("please enter the students marks", mark);
-if (mark < 79){
+/code to display grades of marks entered by the users
+let mark= prompt("please enter the students marks", mark); //this line prompt the user to enter the students mark
+if (mark > 79)//condition to check if mark entered is more than 79
+{
     console.log("Grade is A");      
     } 
    else if (mark<=79 && mark>60) {
@@ -13,8 +14,9 @@ if (mark < 79){
           console.log("Grade is D"); 
     } else if (mark<40) {
           console.log("Grade is E"); }
-      else if (mark<100 && mark<0) {
+      else if (mark<100 && mark<0)////checks if mark entered is within the range of 0 to 100,
+       {
             console.log("Invalid mark");}
 else {
-      console.log("try again");
+      console.log("try again"); //displays try again if mark entered is not within the conditions eg. entering string instead of integer
     }
